561f3c10bbe5d0ea91f70245901649dac8d043f9
Yeowoon <woons.2016@gmail.com>
2018-08-31 22:52:33 +0900

    updated

--- code.R
+++ code.R
@@ -1,3 +1,43 @@
+library(tidyverse)
+library(ggthemes)
+
+#--------------------------------------------------------------
+# coordinates : coord_cartesian
+#--------------------------------------------------------------
+p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point(alpha = 0.6) +
+  geom_smooth()
+
+p + scale_x_continuous(limits = c(4.5, 5.5)) # scale_x_continuous
+p + xlim(c(4.5, 5.5)) # xlim
+p + coord_cartesian(xlim = c(4.5, 5.5)) # coord_cartesian
+
+#--------------------------------------------------------------
+# coordinates : coord_flip
+#--------------------------------------------------------------
+jeonpo <- read_csv("cafe_jeonpo.csv")
+
+# 데이터저널리즘 기사는 데스크탑에만 보는게 아니죠. 그럴때 coord_flip!
+ggplot(jeonpo, aes(x = reorder(name, -`2017`), y = `2017`)) +
+  geom_bar(stat = "identity") +
+  theme_fivethirtyeight(base_family = "NanumGothic") +
+  scale_y_continuous(labels = scales::comma)
+
+# coord_flip은 모바일에서 보여줄 때 효과적이다
+jeonpo %>% 
+  filter(category == "카페" | category == "디저트" | category == "가게") %>% 
+ggplot(aes(x = reorder(name, `2017`), y = `2017`, fill = category)) +
+  geom_bar(stat = "identity") +
+  theme_fivethirtyeight(base_family = "NanumGothic") +
+  scale_y_continuous(labels = scales::comma) +
+  labs(title = "부산 전포동 공시지가 비교", 
+       subtitile = "카페, 디저트, 가게 중심으로", 
+       caption = "출처 : 정보공개청구") +
+  coord_flip()
+
+#--------------------------------------------------------------
+# facet을 알아보자
+#--------------------------------------------------------------
 cafe <- read_csv("cafe.csv")
 head(cafe)
 
